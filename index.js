490d140be894fa18d8a979476f3e83a588aa70a8
will-17173 <huangzhaojian@gmail.com>
2017-10-17 12:28:41 +0800

    bug fix

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var https = require('https');
 var url = 'https://item.m.jd.com/ware/getDetailCommentList.json?wareId=3867555';
 var fs = require('fs');
 var path = require('path');
+var schedule = require('node-schedule');
+
+var rule = new schedule.RecurrenceRule();
+var hours = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22];
+var min = 0;
+// var sec = [1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56];
+rule.hour = hours;
+rule.minute = min;
+// rule.second = sec;
 
 var db = {};
 
@@ -20,7 +29,7 @@ var year = new Date().getFullYear(),
 	month = new Date().getMonth() + 1,
 	date = new Date().getDate(),
 	day = new Date().getDay() ? (new Date().getDay() - 1) : 6,
-	hour = new Date().getHours(),
+	
 	weekNumber = '' + year + getWeekNumber(year, month, date);
 
 function getCount(){
@@ -47,6 +56,9 @@ function getCount(){
 					[0,0,0,0,0,0,0,0,0,0,0,0]
 				];
 			}
+			
+			var hour = new Date().getHours();
+			console.log(hour)
 			db[weekNumber][day][Math.floor(hour / 2)] = allCnt;
 			fs.writeFile(__dirname + '/db.js', JSON.stringify(db), {}, function(err, res){
 				if(err) {
@@ -59,6 +71,10 @@ function getCount(){
 	})
 }
 
+var j = schedule.scheduleJob(rule, function(){
+	console.log('excuted');
+	getCount();
+})
 
-getCount();
-setInterval(getCount, 72e5);
+// getCount();
+// setInterval(getCount, 72e5);
